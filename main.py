44a5602dd6d0953ed72377b8784330ea6c336557
zheruixS2023 <zheruix@andrew.cmu.edu>
2023-05-01 22:39:59 -0700

    Add input cases

--- main.py
+++ main.py
@@ -6,6 +6,15 @@ def runner(model_name):
     py_model.run_model(model_name)
 
 if __name__ == '__main__':
-    print("Please enter the model name you want to run (SVM, SVM_OVA ,RF, DT, CART, NB, NN):")
-    model_name = input()
-    runner(model_name)
+    while(1):
+        print("Please enter the model name you want to run (SVM, SVM_OVA ,RF, DT, CART, NB, NN):")
+        model_name = input()
+        if model_name not in ['SVM', 'SVM_OVA' ,'RF', 'DT', 'CART', 'NB', 'NN']:
+            print("Please enter the correct model name!")
+            continue
+        runner(model_name)
+        exit_flag = input("Do you want to exit? (y/n)")
+        if exit_flag == 'y':
+            break
+        else:
+            continue
